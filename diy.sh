6ef48ebd584c33b8999b65addfc428351123faae
fyglinfo <41110525+fyglinfo@users.noreply.github.com>
2020-02-26 23:50:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
-src-git lienol https://github.com/Lienol/openwrt-package
